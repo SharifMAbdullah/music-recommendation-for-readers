b42978762f54063302fba23df9df0175cd3952ec
Abhijit Paul <bsse1201@iit.du.ac.bd>
2023-07-07 18:24:12 +0600

    connected em

--- main.js
+++ main.js
@@ -1,7 +1,43 @@
 const ocr = require('./ocr/ocr.js');
 const sentiment = require('./topic_modeling/sentiment.js')
 
-text = ocr.getTextFromImage('./ocr/eng.png')
-    .then((text)=>{
-	console.log(sentiment.get_sentiment(text))
-    });
+
+
+
+
+
+const express = require('express')
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express()
+app.use(cors());
+//app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/api/v1/sentiment/upload/image', (req, res)=>{
+	console.log('Got the request!');
+	console.log(req.body);
+	res.send({'Status':'Working!'});
+});
+
+app.get('/api/v1/sentiment', (req, res) => {
+    text = ocr.getTextFromImage('./ocr/eng.png')
+	.then((text)=>{
+	    sentiment.get_sentiment(text)
+		.then((data)=>{
+		    res.send({'Text': text, 'Sentiment': data});
+		})
+	});
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
--- topic_modeling/sentiment.js
+++ topic_modeling/sentiment.js
@@ -1,18 +1,17 @@
 const axios = require('axios');
 
 
-function get_sentiment(text){
+async function get_sentiment(text){
     const apikey = "TX72hysrCDR39QxMvHSpIcDM2q7lJNZR";
     
-    axios.post("https://api.apilayer.com/sentiment/analysis", {
+    const sentiment = await axios.post("https://api.apilayer.com/sentiment/analysis", {
 	body: text
     }, {
 	headers: {
 	    'apikey': apikey
 	}
-    })
-	.then((response) => console.log(response.data))
-	.catch((error) => console.log("error", error));
+    });
+    return sentiment.data;
 }
 
 
