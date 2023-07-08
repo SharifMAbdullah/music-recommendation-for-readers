803ea121ff7fd63293688416a98a29ebf2d27a82
SharifMAbdullah <bsse1211@iit.du.ac.bd>
2023-07-09 00:33:00 +0600

    ok

--- topic_modeling/app.js
+++ topic_modeling/app.js
@@ -1,7 +1,7 @@
 var myHeaders = new Headers();
 myHeaders.append("apikey", "TX72hysrCDR39QxMvHSpIcDM2q7lJNZR");
 
-var raw = "your eyes they shine so bright";
+var raw = "people often forget that world war two was no joke, indeed it was the utter humiliation of humankind. Such was the  nature of the war that its ruthlessness exceeded anything a man could have conjured up before the war.";
 
 var requestOptions = {
   method: "POST",
@@ -10,7 +10,7 @@ var requestOptions = {
   body: raw,
 };
 
-fetch("https://api.apilayer.com/sentiment/analysis", requestOptions)
+fetch("https://api.apilayer.com/keyword", requestOptions)
   .then((response) => response.text())
   .then((result) => console.log(result))
   .catch((error) => console.log("error", error));
